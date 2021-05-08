316c4dfd103c96f2f713ee23e2c5cf82811d2ca0
Jasin Yip <yejunxing@gmail.com>
2021-05-08 14:19:29 +0800

    feat: get through with Binance WebSocket API.

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,30 @@ import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+const wsClient = new WebSocket('wss://stream.binance.com:9443/ws/adausdt@aggTrade')
+
+wsClient.addEventListener('open', () => console.log('socket: open'))
+wsClient.addEventListener('error', (err) => console.error('socket: error', err))
+wsClient.addEventListener('message', (msg) => console.log('socket: message, msg:', msg))
+wsClient.addEventListener('close', () => console.log('socket: close'))
+
+function sendSubscribe () {
+  wsClient.send(JSON.stringify({
+    method: 'SUBSCRIBE',
+    params: [
+      'btcusdt@aggTrade',
+      'btcusdt@depth',
+    ],
+    id: 1
+  }))
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <button onClick={sendSubscribe}>send</button>
     </div>
   )
 }
