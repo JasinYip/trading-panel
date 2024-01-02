8c5d85cdea479d17609af807cf697a1e88422a63
Jasin Yip <yejunxing@gmail.com>
2024-01-02 10:19:35 +0800

    chore: add the 'base' config for deploying to Github Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  base: '/trading-panel/',
   build: {
     sourcemap: true,
     outDir: 'docs',
