a4b80feefa0e947f951ea7a7b2f7ba7b3aa6ec73
Jasin Yip <yejunxing@gmail.com>
2024-03-15 13:43:32 +0800

    feat: auto refresh.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import SpreadRank from './Components/SpreadRank'
 import PricesSubscriber from './modules/PricesSubscriber'
@@ -7,13 +7,27 @@ import SpreadsProvider from './modules/SpreadsProvider'
 const pricesSubscriber = new PricesSubscriber()
 const spreadsProvider = new SpreadsProvider(pricesSubscriber)
 
-function App() {
+const REFRESH_INTERVAL = 3600; // 1 hour
+const formatTime = (time: number) => (time < 10 ? `0${time}` : time);
 
-  return (
-    <div className="App" style={{ padding: 10 }}>
-      <SpreadRank spreadsProvider={spreadsProvider} />
-    </div>
-  )
+function App() {
+  const [countdown, setCountdown] = useState(REFRESH_INTERVAL)
+
+  useEffect(() => {
+    setInterval(() => {
+      setCountdown((prevCountdown) => {
+        if (prevCountdown <= 1) {
+          window.location.reload(); // 刷新页面
+        }
+        return prevCountdown - 1;
+      });
+    }, 1000)
+  }, [])
+
+  return (<>
+    <SpreadRank spreadsProvider={spreadsProvider} />
+    <span >Auto refresh in {formatTime(Math.floor(countdown / 60))}:{formatTime(countdown % 60)}</span>
+  </>)
 }
 
 export default App
